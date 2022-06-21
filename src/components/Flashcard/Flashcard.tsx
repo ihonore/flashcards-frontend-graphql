74b695909636a11c71ce87d838a7e000d1b81547
Honore IRADUKUNDA <ihonore01@gmail.com>
2022-06-21 17:22:13 +0200

    designed flashcards UI

--- src/components/Flashcard/Flashcard.tsx
+++ src/components/Flashcard/Flashcard.tsx
@@ -1,3 +1,6 @@
+import { CheckCircle, Delete, Edit, Person } from '@mui/icons-material';
+import { Box, Divider, Stack, Typography } from '@mui/material';
+import moment from 'moment';
 import React, { useState, useRef } from 'react';
 
 export interface flashcard {
@@ -13,17 +16,88 @@ export default function Flashcard({ flashcard }: any) {
   const backEl = useRef() as React.MutableRefObject<HTMLDivElement>;
 
   return (
-    <div
-      className={`card ${flip ? 'flip' : ''}`}
-      style={{ height: 250, width: 400 }}
-      onClick={() => setFlip(!flip)}
+    <Box
+      sx={{
+        position: 'relative',
+        height: { xs: '25vh', sm: 250 },
+        width: { xs: '90vw', sm: 400 },
+      }}
     >
-      <div className="front" ref={frontEl}>
-        {flashcard.question}
-      </div>
-      <div className="back" ref={backEl}>
-        {flashcard.answer}
-      </div>
-    </div>
+      {!flip && (
+        <div>
+          <Box
+            sx={{
+              position: 'absolute',
+              top: '1.5rem',
+              right: { xs: '0.2rem', sm: '0.5rem' },
+              backgroundColor: 'white',
+              padding: '0.5rem 0.5rem',
+              zIndex: 2,
+            }}
+          >
+            <Stack
+              sx={{
+                backgroundColor: '#F6CA8B',
+                boxShadow: '4px 4px 4px rgba(0, 0, 0, 0.2)',
+                borderRadius: '7px',
+                cursor: 'pointer',
+              }}
+            >
+              <CheckCircle
+                sx={{
+                  color: flashcard.isDone ? '#26A716' : 'white',
+                  padding: '4px 2px',
+                }}
+              />
+              <Divider />
+              <Edit sx={{ color: '#2E91D4', padding: '4px 2px' }} />
+              <Divider />
+              <Delete sx={{ color: '#FF7247', padding: '4px 2px' }} />
+            </Stack>
+          </Box>
+          <Box
+            sx={{
+              position: 'absolute',
+              bottom: '0.2rem',
+              right: '0.5rem',
+              zIndex: 2,
+              // backgroundColor: 'white',
+              // padding: '0.5rem 0.5rem',
+            }}
+          >
+            <Stack direction="row" gap={2}>
+              <Box display="flex">
+                <Person fontSize="small" sx={{ color: '#90C5A9' }} />
+                <Typography sx={{ fontSize: '0.9rem', color: '#637AAC' }}>
+                  {flashcard.postedBy.email.split('@')[0]}
+                </Typography>
+              </Box>
+              <Typography sx={{ fontSize: '0.9rem', color: '#A990D3' }}>
+                {moment(flashcard.createdAt).fromNow()}
+              </Typography>
+            </Stack>
+          </Box>
+        </div>
+      )}
+      <Box
+        className={`card ${flip ? 'flip' : ''}`}
+        sx={{ height: { xs: '25vh', sm: 250 }, width: { xs: '90vw', sm: 400 } }}
+        onClick={() => setFlip(!flip)}
+      >
+        <Box className="front" ref={frontEl}>
+          <Typography
+            variant="h6"
+            sx={{
+              fontSize: { xs: '1rem', sm: '1.2rem' },
+            }}
+          >
+            {flashcard.question}
+          </Typography>
+        </Box>
+        <div className="back" ref={backEl}>
+          <Typography variant="h6">{flashcard.answer}</Typography>
+        </div>
+      </Box>
+    </Box>
   );
 }
--- src/components/FlashcardsList/FlashcardsList.tsx
+++ src/components/FlashcardsList/FlashcardsList.tsx
@@ -1,23 +1,105 @@
 import React from 'react';
-import { useFlashcardsQuery } from '../../generated/graphql';
+// import { useFlashcardsQuery } from '../../generated/graphql';
 import Flashcard from '../Flashcard/Flashcard';
 
 const FlashcardaList: React.FC = () => {
-  const { data, error, loading } = useFlashcardsQuery();
+  // const { data, error, loading } = useFlashcardsQuery();
 
-  if (loading) {
-    return <div style={{ color: 'white', fontSize: '2rem' }}>loading...</div>;
-  }
-  if (error || !data) {
-    return (
-      <div style={{ color: 'white', fontSize: '2rem' }}>
-        Something went wrong
-      </div>
-    );
-  }
+  // if (loading) {
+  //   return <div style={{ color: 'white', fontSize: '2rem' }}>loading...</div>;
+  // }
+  // if (error || !data) {
+  //   return (
+  //     <div style={{ color: 'white', fontSize: '2rem' }}>
+  //       Something went wrong
+  //     </div>
+  //   );
+  // }
+  const flashcards = [
+    {
+      id: 1,
+      question: 'How is the weather up there?',
+      answer: 'Too bad!',
+      isDone: true,
+      postedBy: {
+        email: 'ihonore01@gmail.com',
+      },
+      createdAt: '2022-06-20T06:57:18.497Z',
+    },
+    {
+      id: 2,
+      question: 'How is the weather up there?',
+      answer: 'Too bad!',
+      isDone: false,
+      postedBy: {
+        email: 'ihonore01@gmail.com',
+      },
+      createdAt: '2022-06-20T06:57:18.497Z',
+    },
+    {
+      id: 3,
+      question: 'How is the weather up there?',
+      answer: 'Too bad!',
+      isDone: false,
+      postedBy: {
+        email: 'ihonore01@gmail.com',
+      },
+      createdAt: '2022-06-20T06:57:18.497Z',
+    },
+    {
+      id: 4,
+      question: 'How is the weather up there?',
+      answer: 'Too bad!',
+      isDone: true,
+      postedBy: {
+        email: 'ihonore01@gmail.com',
+      },
+      createdAt: '2022-06-20T06:57:18.497Z',
+    },
+    {
+      id: 5,
+      question: 'How is the weather up there?',
+      answer: 'Too bad!',
+      isDone: true,
+      postedBy: {
+        email: 'ihonore01@gmail.com',
+      },
+      createdAt: '2022-06-20T06:57:18.497Z',
+    },
+    {
+      id: 6,
+      question: 'How is the weather up there?',
+      answer: 'Too bad!',
+      isDone: false,
+      postedBy: {
+        email: 'ihonore01@gmail.com',
+      },
+      createdAt: '2022-06-20T06:57:18.497Z',
+    },
+    {
+      id: 7,
+      question: 'How is the weather up there?',
+      answer: 'Too bad!',
+      isDone: false,
+      postedBy: {
+        email: 'ihonore01@gmail.com',
+      },
+      createdAt: '2022-06-20T06:57:18.497Z',
+    },
+    {
+      id: 8,
+      question: 'How is the weather up there?',
+      answer: 'Too bad!',
+      isDone: true,
+      postedBy: {
+        email: 'ihonore01@gmail.com',
+      },
+      createdAt: '2022-06-20T06:57:18.497Z',
+    },
+  ];
   return (
     <div className="card-grid">
-      {data.flashcards.flashcards.map((flashcard) => {
+      {flashcards.map((flashcard) => {
         return <Flashcard flashcard={flashcard} />;
       })}
     </div>
